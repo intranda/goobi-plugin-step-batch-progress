88bd2671e5c8a387a9ced65770b1167dacc5806b
Robert Sehr <robert.sehr@intranda.com>
2021-09-24 11:51:34 +0200

    check if all processes of a batch have reached a step, close them

--- plugin/src/main/java/de/intranda/goobi/plugins/BatchProgressStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/BatchProgressStepPlugin.java
@@ -1,5 +1,7 @@
 package de.intranda.goobi.plugins;
 
+import java.util.ArrayList;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,7 +22,9 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.List;
 
+import org.goobi.beans.Process;
 import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
@@ -28,6 +32,8 @@ import org.goobi.production.enums.PluginType;
 import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
 
+import de.sub.goobi.helper.HelperSchritte;
+import de.sub.goobi.persistence.managers.ProcessManager;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
@@ -46,10 +52,13 @@ public class BatchProgressStepPlugin implements IStepPluginVersion2 {
     private boolean allowTaskFinishButtons;
     private String returnPath;
 
+    private Process process;
+
     @Override
     public void initialize(Step step, String returnPath) {
         this.returnPath = returnPath;
         this.step = step;
+        process = step.getProzess();
     }
 
     @Override
@@ -93,28 +102,64 @@ public class BatchProgressStepPlugin implements IStepPluginVersion2 {
     @Override
     public boolean execute() {
         PluginReturnValue ret = run();
-        return ret != PluginReturnValue.ERROR;
+        return ret == PluginReturnValue.FINISH;
     }
 
     @Override
     public PluginReturnValue run() {
-        boolean successfull = true;
-        // your logic goes here
 
-        // check if process belongs to a batch
-        // check if all other processes of the batch reached this step
-
-        // if no - wait
-
-        // if yes -> call api
-
-        // if yes -> close the step in all other processes
-
-
-        log.info("BatchProgress step plugin executed");
-        if (!successfull) {
-            return PluginReturnValue.ERROR;
+        // check if process belongs to a batch, if not
+        if (process.getBatch() == null) {
+            // process doesn't belong to a batch, continue
+            return PluginReturnValue.FINISH;
         }
+
+        List<Step> stepsToClose = new ArrayList<>();
+        List<Integer> processesInBatch = ProcessManager.getIDList("batchId=" + process.getBatch().getBatchId());
+        for (Integer processId : processesInBatch) {
+            if (processId.equals(process.getId())) {
+                // skip current process
+                continue;
+            }
+
+            // check if all other processes of the batch reached this step
+            Process otherProcessInBatch = ProcessManager.getProcessById(processId);
+            for (Step otherstep : otherProcessInBatch.getSchritte()) {
+                if (step.getTitel().equals(otherstep.getTitel())) {
+                    // found step to check
+                    switch (otherstep.getBearbeitungsstatusEnum()) {
+                        case LOCKED:
+                            // if no - wait
+                            return PluginReturnValue.WAIT;
+                        case ERROR:
+                        case INFLIGHT:
+                        case INWORK:
+                        case OPEN:
+                            // must be closed at the end
+                            stepsToClose.add(otherstep);
+                            break;
+                        case DEACTIVATED:
+                        case DONE:
+                        default:
+                            // already got further, do nothing
+                            break;
+
+                    }
+                }
+            }
+        }
+
+        // we reached this, so we don't have any locked steps
+        // TODO call rest api
+
+        // close the step in all other processes
+        if (!stepsToClose.isEmpty()) {
+            HelperSchritte hs = new HelperSchritte();
+            for (Step other : stepsToClose) {
+                hs.CloseStepObjectAutomatic(other);
+            }
+        }
+
         return PluginReturnValue.FINISH;
     }
 }
