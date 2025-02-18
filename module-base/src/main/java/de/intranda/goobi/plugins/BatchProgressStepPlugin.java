3a47dbb067ed633887689687710c9d324ff7caef
Dominick Leppich <dominick.leppich@intranda.com>
2025-02-18 12:04:48 +0100

    fix: Tomcat 10 namespace fixes

--- module-base/src/main/java/de/intranda/goobi/plugins/BatchProgressStepPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/BatchProgressStepPlugin.java
@@ -25,10 +25,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MediaType;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
